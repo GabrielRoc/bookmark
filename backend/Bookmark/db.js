61c01bdb703fd98db7f467f3e30da3f9f81f4066
Gabriel Rocha <gabrielroc2112@gmail.com>
2021-03-30 09:39:07 -0300

    Corrigido conexão ao BD

--- backend/Bookmark/db.js
+++ backend/Bookmark/db.js
@@ -3,7 +3,7 @@ async function connect(){
         return global.connection;
  
     const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection("mysql://gabrielr_projetointegrador:EssaSenhaEspecial@servidor.gabrielrocha.net.br:3306/crud");
+    const connection = await mysql.createConnection("mysql://gabrielr_projetointegrador:EssaSenhaEspecial@servidor.gabrielrocha.net.br:3306/gabrielr_projetointegrador");
     console.log("Conectou no MySQL!");
     global.connection = connection;
     return connection;
